4cb40671d5cc2191ca7fa66cb5db23ebe89eb7d5
Joshua Klein <mobiusklein@gmail.com>
2023-04-22 10:59:46 -0400

    Update versions

--- src/text.rs
+++ src/text.rs
@@ -5,9 +5,21 @@ use std::path;
 
 use crate::arrayops::ArrayPair;
 
+/// A helper function to write an [ArrayPair] to a file on disk as plain text
+/// The written format is a tab-separated file denoting an m/z intensity pair,
+/// with one pair per line.
+/// See [arrays_to_writer]
 pub fn arrays_to_file<P: AsRef<path::Path>>(arrays: ArrayPair<'_>, path: P) -> io::Result<()> {
     let file = fs::File::create(path)?;
     let mut writer = io::BufWriter::new(file);
+    arrays_to_writer(arrays, &mut writer)
+}
+
+
+/// A helper function to write an [ArrayPair] to a [Writer] as plain text
+/// The written format is a tab-separated file denoting an m/z intensity pair,
+/// with one pair per line.
+pub fn arrays_to_writer<W: io::Write>(arrays: ArrayPair<'_>, writer: &mut W) -> io::Result<()> {
     let n = arrays.len();
     for i in 0..n {
         let pt = arrays.get(i).unwrap();
@@ -16,6 +28,10 @@ pub fn arrays_to_file<P: AsRef<path::Path>>(arrays: ArrayPair<'_>, path: P) -> i
     Ok(())
 }
 
+
+/// A helper function to read an [ArrayPair] from a [Reader].
+/// The expected format is a tab-separated file denoting an m/z intensity pair,
+/// with one pair per line.
 pub fn arrays_from_reader<'a, R: io::Read>(source: R) -> io::Result<ArrayPair<'a>> {
     let reader = io::BufReader::new(source);
     let mut mz_array: Vec<f64> = Vec::new();
@@ -35,6 +51,11 @@ pub fn arrays_from_reader<'a, R: io::Read>(source: R) -> io::Result<ArrayPair<'a
     Ok(ArrayPair::from((mz_array, intensity_array)))
 }
 
+
+/// A helper function to read an [ArrayPair] from a file on disk.
+/// The expected format is a tab-separated file denoting an m/z intensity pair,
+/// with one pair per line.
+/// See [arrays_from_reader]
 pub fn arrays_from_file<'a, P: AsRef<path::Path>>(path: P) -> io::Result<ArrayPair<'a>> {
     let reader = fs::File::open(path)?;
     arrays_from_reader(reader)
