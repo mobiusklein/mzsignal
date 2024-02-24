0332bfdb71db158976ba36f4e3c8055c542cf160
Joshua Klein <mobiusklein@gmail.com>
2024-02-24 11:02:36 -0500

    Kind of working savgol ndarray

--- src/smooth.rs
+++ src/smooth.rs
@@ -162,7 +162,7 @@ pub fn moving_average_dyn<F: Float + AddAssign + SubAssign>(xdata: &[F], xout: &
 }
 
 /// All the ways a Savitsky-Golay filter can go wrong
-#[derive(Debug, Clone, Copy, Error)]
+#[derive(Debug, Clone, Error)]
 pub enum SavitskyGolayError {
     #[error("The window length must be an odd number, received {0}")]
     WindowLengthNotOdd(usize),
@@ -173,7 +173,7 @@ pub enum SavitskyGolayError {
     #[error("The polynomial order term {0} must be less than the window size {1}")]
     PolynomialOrderTooLarge(usize, usize),
     #[error("Failed to solve for coefficients: {0}")]
-    FailedToSolveCoefficients(&'static str),
+    FailedToSolveCoefficients(String),
 }
 
 
@@ -252,6 +252,203 @@ impl<F: Float + AddAssign + SubAssign> AsRef<[F]> for Polynomial<F> {
     }
 }
 
+#[cfg(feature = "ndarray")]
+mod ndarray_impl {
+    use ndarray::{Array, Array1, Array2};
+    use ndarray_linalg::{Lapack, Scalar, Solve, SVD};
+
+    use super::*;
+
+    impl<F: Float + Debug + 'static + Lapack + Scalar> SavitskyGolay<F> {
+        pub fn new(
+            window_length: usize,
+            poly_order: usize,
+            derivative: usize,
+        ) -> Result<Self, SavitskyGolayError> {
+            let mut inst = Self {
+                window_length,
+                poly_order,
+                derivative,
+                coefs: Vec::new(),
+            };
+            inst.validate()?;
+            inst.coefs = inst.estimate_coefficients()?;
+            Ok(inst)
+        }
+
+        fn validate(&self) -> Result<(), SavitskyGolayError> {
+            if self.window_length % 2 == 0 {
+                Err(SavitskyGolayError::WindowLengthNotOdd(self.window_length))
+            } else if self.poly_order >= self.window_length {
+                Err(SavitskyGolayError::PolynomialOrderTooLarge(
+                    self.poly_order,
+                    self.window_length,
+                ))
+            } else {
+                Ok(())
+            }
+        }
+
+        fn estimate_coefficients(&self) -> Result<Vec<F>, SavitskyGolayError> {
+            let half_length = self.window_length / 2;
+            let rem = self.window_length % 2;
+
+            let pos = match rem {
+                0 => F::from(half_length).unwrap() - F::from(0.5).unwrap(),
+                _ => F::from(half_length).unwrap(),
+            };
+            if self.derivative > self.poly_order {
+                let mut coefs = Vec::with_capacity(self.window_length);
+                coefs.resize(self.window_length, F::zero());
+                Ok(coefs)
+            } else {
+                // There is an error in here somewhere
+
+                // Construct a Vandermond matrix
+                let x = Array1::from_iter((0..self.window_length).into_iter().map(|i| pos - F::from(i).unwrap()));
+                let order = Array1::from_iter((0..(self.poly_order + 1)).into_iter().map(|i| F::from(i).unwrap()));
+
+                let vandermond = Array2::from_shape_fn((self.poly_order + 1, self.window_length), |(i, j)| {
+                    Float::powf(x[j], order[i])
+                });
+
+                let mut y = Array1::from_shape_fn(self.poly_order + 1, |_| F::zero());
+                y[self.derivative] = F::from(factorial(self.derivative)).unwrap();
+
+
+                let (u, _s, _v) = vandermond.svd(true, true).unwrap();
+                let u = u.unwrap();
+                let beta = u.dot(&u.t()).dot(&y);
+
+                Ok(beta.to_vec())
+            }
+        }
+
+        const fn half_length(&self) -> usize {
+            self.window_length / 2
+        }
+
+        fn polyfit(&self, x: &[F], y: &[F]) -> Result<Vec<F>, SavitskyGolayError> {
+            let nc = self.poly_order + 1;
+            let nr = x.len();
+
+            // Initialize system of equations for polynomial
+            let mut system = Array2::zeros((nr, nc));
+            x.iter().enumerate().for_each(|(row_i, x)| {
+                system[(row_i, 0)] = F::one();
+                (1..nc).for_each(|col_j| {
+                    system[(row_i, col_j)] = Float::powf(*x, F::from(col_j).unwrap())
+                });
+            });
+
+            let beta = Array1::from_iter(y.iter().copied());
+
+            let (u, _s, _v) = system.svd(true, true).unwrap();
+            let u = u.unwrap();
+
+            let poly_coefs = u.dot(&u.t()).dot(&beta);
+            Ok(poly_coefs.to_vec())
+        }
+
+        fn fit_edge(
+            &self,
+            x: &Array1<F>,
+            window_start: usize,
+            window_stop: usize,
+            interp_start: usize,
+            interp_stop: usize,
+            y: &mut Vec<F>,
+        ) -> Result<(), SavitskyGolayError> {
+            let x_edge = &x.as_slice().unwrap()[window_start..window_stop];
+            let y_edge: Vec<_> = (0..window_stop - window_start)
+                .map(|i| F::from(i).unwrap())
+                .collect();
+
+            let poly_coefs = Polynomial::new(self.polyfit(x_edge, &y_edge)?, self.poly_order);
+            let poly_coefs = poly_coefs.derivative_to_zero(self.derivative);
+            let i: Vec<_> = (0..interp_stop - interp_start)
+                .map(|i| F::from(interp_start - window_start + i).unwrap())
+                .collect();
+            let values = poly_coefs.eval(&i);
+            y.splice(interp_start..interp_stop, values);
+            Ok(())
+        }
+
+        fn fit_edges(&self, x: &Array1<F>, y: &mut Vec<F>) -> Result<(), SavitskyGolayError> {
+            self.fit_edge(
+                x,
+                0,
+                self.window_length,
+                self.half_length(),
+                self.poly_order,
+                y,
+            )?;
+            let n = x.len();
+            self.fit_edge(x, n - self.window_length, n, n - self.half_length(), n, y)
+        }
+
+        fn convolve(&self, x: &Array1<F>, coefs: &Array1<F>) -> Array1<F> {
+            let vec = x.len();
+            let ker = coefs.len();
+
+            let result_len = (x.shape()[0] + coefs.shape()[0]) - 1;
+            let mut conv = Array1::<F>::zeros([result_len]);
+            for i in 0..vec + ker - 1 {
+                let u_i = if i > vec { i - ker } else { 0 };
+                let u_f = i.min(vec - 1);
+                if u_i == u_f {
+                    conv[i] += x[u_i] * coefs[i - u_i];
+                } else {
+                    for u in u_i..u_f + 1 {
+                        if i - u < ker {
+                            conv[i] += x[u] * coefs[i - u];
+                        }
+                    }
+                }
+            }
+            conv
+        }
+
+        pub fn smooth(&self, data: &[F]) -> Result<Vec<F>, SavitskyGolayError> {
+            let n = data.len();
+            if self.window_length > n {
+                return Err(SavitskyGolayError::WindowLengthTooLong(
+                    self.window_length,
+                    n,
+                ));
+            }
+            let coefs = Array1::from_iter(self.coefs.iter().copied());
+            let x = Array1::from_iter(data.iter().copied());
+            // This seems to be a bottleneck
+            let y = self.convolve(&x, &coefs);
+            let padding = (y.len() - x.len()) / 2;
+            let y = y.as_slice().unwrap();
+            let mut y = y[padding..y.len().saturating_sub(padding)].to_vec();
+            self.fit_edges(&x, &mut y)?;
+            let zero = F::zero();
+            y.iter_mut().for_each(|y| {
+                if *y < zero {
+                    *y = zero;
+                }
+            });
+            Ok(y)
+        }
+    }
+
+    /// A wrapper around [`SavitskyGolay`] for the [`ndarray`] backend.
+    pub fn savitsky_golay<
+        F: Float + Debug + 'static + Lapack + Scalar,
+    >(
+        data: &[F],
+        window_length: usize,
+        poly_order: usize,
+        derivative: usize,
+    ) -> Result<Vec<F>, SavitskyGolayError> {
+        let state = SavitskyGolay::new(window_length, poly_order, derivative)?;
+        state.smooth(data)
+    }
+}
+
 #[cfg(feature = "nalgebra")]
 mod nalgebra_impl {
     use nalgebra::{DMatrix, DVector};
@@ -259,7 +456,7 @@ mod nalgebra_impl {
     use super::*;
 
     impl<F: Float + Debug + 'static + nalgebra::ComplexField + nalgebra::RealField> SavitskyGolay<F> {
-        fn new(
+        pub fn new(
             window_length: usize,
             poly_order: usize,
             derivative: usize,
@@ -314,7 +511,7 @@ mod nalgebra_impl {
                 let svd = nalgebra::linalg::SVD::new(vandermond, true, true);
                 let beta = match svd.solve(&y, F::from(1e-12).unwrap()) {
                     Ok(val) => val,
-                    Err(err) => return Err(SavitskyGolayError::FailedToSolveCoefficients(err)),
+                    Err(err) => return Err(SavitskyGolayError::FailedToSolveCoefficients(err.to_string())),
                 };
                 Ok(beta.into_iter().copied().collect())
             }
@@ -343,7 +540,7 @@ mod nalgebra_impl {
             // Solve system of equations for polynomial coefficients
             let poly_coefs: Vec<_> = match decomp.solve(&beta, F::from(1e-18).unwrap()) {
                 Ok(val) => val.data.into(),
-                Err(e) => return Err(SavitskyGolayError::FailedToSolveCoefficients(e)),
+                Err(e) => return Err(SavitskyGolayError::FailedToSolveCoefficients(e.to_string())),
             };
             Ok(poly_coefs)
         }
@@ -428,6 +625,10 @@ mod nalgebra_impl {
 #[cfg(feature = "nalgebra")]
 pub use nalgebra_impl::savitsky_golay;
 
+#[cfg(not(feature = "nalgebra"))]
+#[cfg(feature = "ndarray")]
+pub use ndarray_impl::savitsky_golay;
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -438,6 +639,7 @@ mod test {
         io::{self, Write},
     };
 
+    #[cfg(feature = "nalgebra")]
     #[test]
     fn test_savgol() -> io::Result<()> {
         let actual_y: Vec<_> = Y
@@ -445,7 +647,26 @@ mod test {
             .zip(NOISE.iter())
             .map(|(y, o)| *y * 1000.0 + *o)
             .collect();
-        let smoothed = savitsky_golay(&actual_y, 5, 3, 0).unwrap();
+
+        let filter = SavitskyGolay::<f32>::new(5, 3, 0).unwrap();
+        let smoothed = filter.smooth(&actual_y).unwrap();
+
+        match fs::create_dir("tmp") { Ok(_) => {}, Err(_) => {} };
+
+        let mut coeffh = fs::File::create("tmp/coefs.txt")?;
+        filter.coefs.iter().for_each(|y| {
+            coeffh.write(format!("{}\n", *y).as_bytes()).unwrap();
+        });
+
+        let mut rawfh = fs::File::create("tmp/raw.txt")?;
+        actual_y.iter().for_each(|y| {
+            rawfh.write(format!("{}\n", *y).as_bytes()).unwrap();
+        });
+        let mut rawfh = fs::File::create("tmp/savgol.txt")?;
+        smoothed.iter().for_each(|y| {
+            rawfh.write(format!("{}\n", *y).as_bytes()).unwrap();
+        });
+
         let diff: f32 = actual_y
             .iter()
             .zip(smoothed.iter())
@@ -459,15 +680,6 @@ mod test {
             "Difference {diff} was {} (too large)",
             (diff - 1761.9003).abs()
         );
-
-        // let mut rawfh = fs::File::create("raw.txt")?;
-        // actual_y.iter().for_each(|y| {
-        //     rawfh.write(format!("{}\n", *y).as_bytes()).unwrap();
-        // });
-        // let mut rawfh = fs::File::create("savgol.txt")?;
-        // smoothed.iter().for_each(|y| {
-        //     rawfh.write(format!("{}\n", *y).as_bytes()).unwrap();
-        // });
         Ok(())
     }
 }
